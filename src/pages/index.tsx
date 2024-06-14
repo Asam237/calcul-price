881e2089cc4790743898414548e445b5834454f9
Asam <abbasaliaboubakar@gmail.com>
2024-06-14 07:52:22 +0100

    add family size

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,6 +17,8 @@ const Home = () => {
   const [priceUKTB, setPriceUKTB] = useState(0);
   const [defaultValue, setDefaultValue] = useState(0);
   const [sel, setSel] = useState(0);
+  const [familySize1, setFamilySize1] = useState(0);
+  const [familySize2, setFamilySize2] = useState(0);
 
   const {
     register,
@@ -32,12 +34,22 @@ const Home = () => {
     const price6 = (parseInt(data.fieldSix) || 0) * 44350;
     setPriceCANIMM(price1 + price2 + price3 + price4);
     setPriceUKTB(price5 + price6);
+    const sizes1 =
+      parseInt(data.fieldOne) +
+      parseInt(data.fieldTwo) +
+      parseInt(data.fieldThree) +
+      parseInt(data.fieldFour);
+    const sizes2 = parseInt(data.fieldFive) + parseInt(data.fieldSix);
+    setFamilySize1(sizes1);
+    setFamilySize2(sizes2);
   };
 
   const toClear = () => {
     setDefaultValue(0);
     setPriceCANIMM(0);
     setPriceUKTB(0);
+    setFamilySize1(0);
+    setFamilySize2(0);
   };
 
   return (
@@ -137,6 +149,11 @@ const Home = () => {
                 defaultValue={defaultValue}
               />
             </div>
+            {familySize1 !== 0 && (
+              <p className="bg-gray-300 text-gray-900 px-2 py-0.5 mt-4 rounded-md text-xs animate-pulse">
+                Family size: {familySize1}
+              </p>
+            )}
             <div className="flex gap-x-2 flex-row w-1/2">
               <button
                 className="bg-blue-600 mt-4 w-full h-8 p-1 rounded-md text-sm"
@@ -191,6 +208,11 @@ const Home = () => {
                 min={0}
               />
             </div>
+            {familySize2 !== 0 && (
+              <p className="bg-gray-300 text-gray-900 px-2 py-0.5 mt-4 rounded-md text-xs animate-pulse">
+                Family size: {familySize2}
+              </p>
+            )}
             <div className="flex gap-x-2 flex-row w-1/2">
               <button
                 className="bg-blue-600 mt-4 w-full h-8 p-1 rounded-md text-sm"
